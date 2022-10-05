495a3b559ebda69ff15458981446336924e9ee6d
Daniela Escobar <daniela.b.escobar@gmail.com>
2022-10-05 08:43:29 +0200

    Add selfie svg

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,7 +1,25 @@
+import Box from '@mui/material/Box'
+import SelfieIcon from './selfie-icon';
+import Typography from '@mui/material/Typography';
+
 function Home() {
     return (
         <>
-        <p>Hello</p>
+        <Box 
+        display="flex"
+        justifyContent="center"
+        >
+            <SelfieIcon />
+        </ Box>
+        <Box
+        display="flex"
+        justifyContent="center"
+        m="2rem"
+        >
+            <Typography variant="h5">
+        Daniela
+      </Typography>
+        </Box>
         </>
     )
 }
