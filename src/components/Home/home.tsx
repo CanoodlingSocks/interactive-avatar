31bec722d6d6a50e576062712af837e4f9b1d9fc
Daniela Escobar <daniela.b.escobar@gmail.com>
2022-10-17 15:42:53 +0200

    Fix img randomizer

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,38 +1,49 @@
 import Box from '@mui/material/Box'
 import SelfiePicker from '../Selfies/selfie-picker';
 import Button from '@mui/material/Button';
-import {useState} from "react";
+import { useState } from "react";
+
 
 function Home() {
 
-   
-const [toggle, setToggle] = useState<boolean>(false);
+    const images = [
+        "Lela",
+        "EmoLela",
+        "AnimuLela"
+    ];
 
-let SelfieType = {
-    togglebtn: toggle
-}
-    
-    return(
-<>
-<Box
-display="flex"
-justifyContent="center"
->
-<SelfiePicker {...SelfieType}/>    
-</Box>
-<Box
-        display="flex"
-        justifyContent="center"
-        m="2rem"
-        >
-<Button 
-variant="contained"
-onClick={() => setToggle(!toggle)}
->
-    Change hair
-    </Button>
-        </Box>
-</>
+    const [image, setImage] = useState<string>();
+
+    let SelfieType = {
+        randomImage: image
+    }
+
+    function randomizeImg() {
+        const randomImage = images[Math.floor(images.length * Math.random())];
+        setImage(randomImage);
+    }
+
+    return (
+        <>
+            <Box
+                display="flex"
+                justifyContent="center"
+            >
+                <SelfiePicker {...SelfieType} />
+            </Box>
+            <Box
+                display="flex"
+                justifyContent="center"
+                m="2rem"
+            >
+                <Button
+                    variant="contained"
+                    onClick={() => randomizeImg()}
+                >
+                    Change hair
+                </Button>
+            </Box>
+        </>
     )
 }
 
--- src/components/Selfies/selfie-picker.tsx
+++ src/components/Selfies/selfie-picker.tsx
@@ -1,18 +1,23 @@
 import SelfieIcon from "./selfie-icon";
 import EmoSelfieIcon from "./emoselfie-icon";
+import AnimuSelfieIcon from "./animuselfie-icon";
 
 type SelfieType = {
     type: SelfieType
-    togglebtn: boolean
+    randomImage: string
 }
 
 const SelfiePicker = (props: any) => {
 
-        if (props.togglebtn === true){
-            return <EmoSelfieIcon />
-            } else {
-                return <SelfieIcon />
-            }
+    //TODO: Change this to a switch case
+
+    if (props.randomImage === "EmoLela") {
+        return <EmoSelfieIcon />
+    } else if (props.randomImage == "Lela") {
+        return <SelfieIcon />
+    } else {
+        return <AnimuSelfieIcon />
+    }
 }
 
 export default SelfiePicker;
